77e064372985186db25a21dcef8319418a87ed74
Arifulislam763 <121458055+Arifulislam763@users.noreply.github.com>
2024-02-14 02:09:34 +0530

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ try: api_hash = os.environ.get("api_hash", "cc64f8b770f0960bb418768de4126336")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
 try: bot_token = os.environ.get("bot_token", "5749543675:AAGQj_Fc-DwohmH3sg7HO6c45T6KJ3CiTDw")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
-try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
+try: custom_caption = os.environ.get("custom_caption", "`{file_name}` @Ariful")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
 AutoCaptionBot = pyrogram.Client(

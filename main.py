6cd98ae64a9933c5afb23ce2e801827618f21073
Arifulislam763 <121458055+Arifulislam763@users.noreply.github.com>
2024-02-14 01:37:53 +0530

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pyrogram, os, asyncio
 
-try: app_id = int(os.environ.get("app_id", None))
+try: app_id = int(os.environ.get("app_id", "9273413")
 except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", None)
+try: api_hash = os.environ.get("api_hash", "cc64f8b770f0960bb418768de4126336")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", None)
+try: bot_token = os.environ.get("bot_token", "5749543675:AAGQj_Fc-DwohmH3sg7HO6c45T6KJ3CiTDw")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
 try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
